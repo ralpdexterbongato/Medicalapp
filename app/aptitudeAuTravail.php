2a78caa21e79dae894be095141ebd2a334ff737f
ralpdexterbongato <rdc2898@gmail.com>
2018-05-11 01:28:48 +0800

    fix bug

--- app/aptitudeAuTravail.php
+++ app/aptitudeAuTravail.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class aptitudeAuTravail extends Model
 {
+	protected $primaryKey = "id_app";
+	
   public function visite() {
     return $this->hasMany(visite::class);
   }
